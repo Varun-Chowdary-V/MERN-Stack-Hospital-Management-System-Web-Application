c8b195299753853dfd9fde248014ff3fba928343
Varun-Chowdary-V <21bq1a05n9.vvit@gmail.com>
2024-11-25 20:57:12 +0530

    Initial commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,46 @@
+import { Link } from "react-router-dom";
+import { FaLocationArrow, FaPhone } from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
+
+const Footer = () => {
+  
+
+  return (
+    <>
+      <footer className={"container"}>
+        <hr />
+        <div className="content">
+          <div>
+            <img src="/logo.png" alt="logo" className="logo-img"/>
+          </div>
+          <div>
+            <h4>Quick Links</h4>
+            <ul>
+              <Link to={"/"}>Home</Link>
+              <Link to={"/appointment"}>Appointment</Link>
+              <Link to={"/about"}>About</Link>
+            </ul>
+          </div>
+          
+          <div>
+            <h4>Contact</h4>
+            <div>
+              <FaPhone />
+              <span>999-999-9999</span>
+            </div>
+            <div>
+              <MdEmail />
+              <span>zeelab@gmail.com</span>
+            </div>
+            <div>
+              <FaLocationArrow />
+              <span>India</span>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
